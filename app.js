d75061db9eabbb811b63df86d5980c1fd034764b
russellpipal <russell.pipal@gmail.com>
2016-04-01 11:47:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var gifObject = {};
+var jGif = "";
+gifUrl = "";
+
+$(function(){
+  $("#getGif").on("click", function(){
+    $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").done(function(response){
+      gifObject = response;
+      gifUrl = gifObject.data.image_url;
+      jGif = "<img src=\"" + gifUrl + "\"alt = \"Random Image\">";
+      console.log(gifObject);
+      $("body").append(jGif);
+    });
+  });
+});
